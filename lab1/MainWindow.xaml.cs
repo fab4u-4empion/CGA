4cda7f1c1c9a3e224cf5269b8868539825ef9788
fab4u_4empion <reserv390@gmail.com>
2023-09-06 17:48:36 +0300

    com1

--- lab1/MainWindow.xaml.cs
+++ lab1/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Numerics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace lab1
 {
@@ -16,24 +17,26 @@ namespace lab1
     public partial class MainWindow : Window
     {
         Pbgra32Bitmap bitmap;
+        List<Point> setedPixels = new List<Point>();
         Model model = new();
+
+        float z = 25;
+        float x = 0;
+        float y = 0;
 
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void ParseModelFromFile(string path)
         {
-            bitmap = new((int)Grid.ActualWidth, (int)Grid.ActualHeight);
-            Canvas.Source = bitmap.Source;
-
-            using (StreamReader reader = new("./model/shovel_low.obj"))
+            using (StreamReader reader = new(path))
             {
                 while (!reader.EndOfStream)
                 {
-                    String line = reader.ReadLine();
+                    String line = reader.ReadLine().Trim();
+
                     if (line.StartsWith("v "))
                     {
                         List<float> coordinates = line
@@ -45,19 +48,39 @@ namespace lab1
                             .ToList();
                         model.AddVertex(coordinates[0], coordinates[1], coordinates[2]);
                     }
+
+                    if (line.StartsWith("f "))
+                    {
+                        model.AddFace(
+                            line
+                                .Remove(0, 2)
+                                .Split(" ")
+                                .Select(v =>
+                                {
+                                    List<int> indexes = v
+                                        .Split("/")
+                                        .Select(i => int.Parse(i, CultureInfo.InvariantCulture))
+                                        .ToList();
+                                    return new Vector3(indexes[0], indexes[1], indexes[2]);
+                                }
+                                )
+                                .ToList()
+                        );
+                    }
                 }
             }
+        }
 
-            Matrix4x4 scaleMatrix = Matrix4x4.CreateScale(1);
-            Matrix4x4 rotationMatrix = Matrix4x4.CreateFromYawPitchRoll(0, 0, 0);
-            Matrix4x4 translationMatrix = Matrix4x4.CreateTranslation(0, 0, 0);
+        private Vector4[] TransformCoordinates()
+        {
+            Matrix4x4 scaleMatrix = Matrix4x4.CreateScale(model.Scale);
+            Matrix4x4 rotationMatrix = Matrix4x4.CreateFromYawPitchRoll(model.Yaw, model.Pitch, model.Roll);
+            Matrix4x4 translationMatrix = Matrix4x4.CreateTranslation(model.Translation);
             Matrix4x4 modelMatrix = scaleMatrix * rotationMatrix * translationMatrix;
-            Matrix4x4 viewMatrix = Matrix4x4.CreateLookAt(new Vector3(0, 5, 30), new Vector3(0, 5, 0), new Vector3(0, 1, 0));
-            Matrix4x4 projectionMatrix = Matrix4x4.CreatePerspectiveFieldOfView(float.Pi / 4, (float)bitmap.PixelWidth / bitmap.PixelHeight, 0.1f, 1000);
+            Matrix4x4 viewMatrix = Matrix4x4.CreateLookAt(new Vector3(x, y, z), new Vector3(x, y, z - 25), new Vector3(0, 1, 0));
+            Matrix4x4 projectionMatrix = Matrix4x4.CreatePerspectiveFieldOfView(float.Pi / 4, (float)bitmap.PixelWidth / (float)bitmap.PixelHeight, 0.1f, 1000);
             Matrix4x4 modelViewProjectionMatrix = modelMatrix * viewMatrix * projectionMatrix;
             Matrix4x4 viewportMatrix = Matrix4x4.CreateViewportLeftHanded(0, 0, bitmap.PixelWidth, bitmap.PixelHeight, 0, 1);
-
-            bitmap.Source.Lock();
 
             Vector4[] windowVertices = new Vector4[model.Vertices.Count];
             for (int i = 0; i < windowVertices.Length; i++)
@@ -65,16 +88,139 @@ namespace lab1
                 windowVertices[i] = Vector4.Transform(model.Vertices[i], modelViewProjectionMatrix);
                 windowVertices[i] /= windowVertices[i].W;
                 windowVertices[i] = Vector4.Transform(windowVertices[i], viewportMatrix);
-                int x = (int)windowVertices[i].X;
-                int y = (int)windowVertices[i].Y;
+            }
+
+            return windowVertices;
+        }
+
+        private void ClearBitmap()
+        {
+            foreach(Point p in setedPixels)
+            {
+                bitmap.ClearPixel((int)p.X, (int)p.Y);
+            }
+        }
+
+        private void DrawLine(Point a, Point b)
+        {
+            double x1 = a.X;
+            double y1 = a.Y;
+            double x2 = b.X;
+            double y2 = b.Y;
+
+            int deltaX = Math.Abs((int)(x1 - x2));
+            int deltaY = Math.Abs((int)(y1 - y2));
+
+            int L = (int)Math.Max(deltaX, deltaY);
+
+            if (L == 0)
+            {
+                if (x1 >= 0 && y1 >= 0 && x1 < bitmap.PixelWidth && y1 < bitmap.PixelHeight)
+                {
+                    bitmap.SetPixel((int)x1, (int)y1, Vector3.Zero);
+                    setedPixels.Add(new(x1, y1));
+                }
+                return;
+            }
+
+            double dX = (x2 - x1) / L;
+            double dY = (y2 - y1) / L;
+
+            double x = x1;
+            double y = y1;
+
+            L++;
+            while (L > 0)
+            {
                 if (x >= 0 && y >= 0 && x < bitmap.PixelWidth && y < bitmap.PixelHeight)
                 {
-                    bitmap.SetPixel((int)windowVertices[i].X, (int)windowVertices[i].Y, Vector3.Zero);
+                    bitmap.SetPixel((int)x, (int)y, Vector3.Zero);
+                    setedPixels.Add(new(x, y));
                 }
+                x += dX;
+                y += dY;
+                L--;
+            }
+        }
+
+        private void Draw()
+        {
+            Vector4[] vertices = TransformCoordinates();
+
+            ClearBitmap();
+
+            setedPixels.Clear();
+
+            bitmap.Source.Lock();
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                int x = (int)vertices[i].X;
+                int y = (int)vertices[i].Y;
+                if (x >= 0 && y >= 0 && x < bitmap.PixelWidth && y < bitmap.PixelHeight)
+                {
+                    bitmap.SetPixel(x, y, Vector3.Zero);
+                    setedPixels.Add(new(x, y));
+                }
+            }
+
+            foreach (List<Vector3> face in model.Faces)
+            {
+                for (int i = 0; i < face.Count - 1; i++)
+                {
+                    DrawLine(
+                        new(vertices[(int)face[i].X - 1].X, vertices[(int)face[i].X - 1].Y),
+                        new(vertices[(int)face[i + 1].X - 1].X, vertices[(int)face[i + 1].X - 1].Y)
+                    );
+                }
+                DrawLine(
+                    new(vertices[(int)face[face.Count - 1].X - 1].X, vertices[(int)face[face.Count - 1].X - 1].Y),
+                    new(vertices[(int)face[0].X - 1].X, vertices[(int)face[0].X - 1].Y)
+                );
             }
 
             bitmap.Source.AddDirtyRect(new(0, 0, bitmap.PixelWidth, bitmap.PixelHeight));
             bitmap.Source.Unlock();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            bitmap = new((int)Grid.ActualWidth, (int)Grid.ActualHeight);
+            Canvas.Source = bitmap.Source;
+            ParseModelFromFile("./model/shovel_low.obj");
+            Draw();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                    z--;
+                    break;
+
+                case Key.S:
+                    z++;
+                    break;
+
+                case Key.A:
+                    x--;
+                    break;
+
+                case Key.D:
+                    x++;
+                    break;
+
+                case Key.Q:
+                    y--;
+                    break;
+
+                case Key.E:
+                    y++;
+                    break;
+            }
+            Draw();
         }
     }
 }
--- lab1/Model.cs
+++ lab1/Model.cs
@@ -10,10 +10,32 @@ namespace lab1
     public class Model
     {
         public List<Vector4> Vertices = new();
+        public List<List<Vector3>> Faces = new();
+
+        public float Scale { get; set; }
+
+        public Vector3 Translation { get; set; }
+
+        public float Yaw { get; set; }
+        public float Pitch { get; set; }
+        public float Roll { get; set; }
+
+        public Model() {
+            Scale = 1.0f;
+            Translation = new Vector3(0, 0, 0);
+            Yaw = 0.0f;
+            Pitch = 0.0f;
+            Roll = 0.0f;
+        }
         
         public void AddVertex(float x, float y, float z)
         {
-            Vertices.Add(new(x, y, x, 1));
+            Vertices.Add(new(x, y, z, 1));
+        }
+
+        public void AddFace(List<Vector3> face)
+        {
+            Faces.Add(face);
         }
     }
 }
